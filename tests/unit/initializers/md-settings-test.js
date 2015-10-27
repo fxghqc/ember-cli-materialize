b3c36c9504505371d3a79985496acc5af11e24e2
fxghqc <konxiao@gmail.com>
2015-10-27 09:07:47 +0800

    remove var container

--- tests/unit/initializers/md-settings-test.js
+++ tests/unit/initializers/md-settings-test.js
@@ -2,13 +2,12 @@ import Ember from 'ember';
 import { initialize } from '../../../initializers/md-settings';
 import { module, test } from 'qunit';
 
-let container, application;
+let application;
 
 module('Unit | Initializer | md settings', {
   beforeEach() {
     Ember.run(function() {
       application = Ember.Application.create();
-      container = application.__container__;
       application.deferReadiness();
     });
   }
@@ -16,7 +15,7 @@ module('Unit | Initializer | md settings', {
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  initialize(container, application);
+  initialize(application);
 
   // you would normally confirm the results of the initializer here
   assert.ok(true);

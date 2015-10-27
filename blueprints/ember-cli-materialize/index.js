7564f95eb5683a4a105b8688846197539bc91dc0
fxghqc <konxiao@gmail.com>
2015-10-27 09:55:01 +0800

    upgrade dependency

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -9,11 +9,11 @@ module.exports = {
 
   afterInstall: function() {
     return RSVP.all([
-      this.addPackageToProject('ember-composability', '~0.1.1'),
+      this.addPackageToProject('ember-composability', '~0.2.1'),
       this.addPackageToProject('ember-radio-button', '1.0.7'),
-      this.addPackageToProject('ember-new-computed', '~1.0.0'),
-      this.addPackageToProject('ember-key-responder', '~0.4.0'),
-      this.addPackageToProject('ember-modal-dialog', '~0.7.5'),
+      this.addPackageToProject('ember-new-computed', '~1.0.3'),
+      this.addPackageToProject('ember-key-responder', '~0.4.2'),
+      this.addPackageToProject('ember-modal-dialog', '~0.8.2'),
       this.addPackageToProject('ember-cli-sass', '^5.0.1'),
       this.addPackageToProject('ember-legacy-views', '^0.2.0')
     ]);
